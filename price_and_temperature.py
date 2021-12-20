144e39e835d6eb8fc64ba555eaaac7c4e60c7dc0
unknown <m13701812479@163.com>
2021-12-20 21:00:06 +0800

    this is my homework

--- price_and_temperature.py
+++ price_and_temperature.py
@@ -4,14 +4,14 @@
 def unit_price():
     # function requirements:
     # the user will input a price (¥), and a weight (kg). This function should return the price per kilogram
-    price = input("What is the price? (¥) ")
-    weight = input("What is the product weight? (kg) ")
-    return f"This product is ¥{1/1}/kg"
+    price = int(input("What is the price? (¥) "))
+    weight = int(input("What is the product weight? (kg) "))
+    return f"This product is ¥{price/weight}/kg"
 
 
 def celsius_to_fahrenheit():
     # function requirements:
     # the user will input a temperature in celsius. This function should return the temperature in Fahrenheit
-    celsius = input("What's the temperature in °C? ")
+    celsius = int(input("What's the temperature in °C? "))
     farenheit = (9 / 5) * celsius + 32
-    return f"°C is °F"
+    return f"{celsius}°C is {farenheit}°F"
--- test_price_and_temperature.py
+++ test_price_and_temperature.py
@@ -4,7 +4,7 @@ from price_and_temperature import unit_price, celsius_to_fahrenheit
 
 class Test(TestCase):
     def test_unit_price(self):
-        self.assertEqual(unit_price(), f"This product is ¥ /kg")
+        self.assertEqual(unit_price(), f"This product is ¥5.0/kg")
 
     def test_celsius_to_fahrenheit(self):
-        self.assertEqual(celsius_to_fahrenheit(), f" °C is  °F")
+        self.assertEqual(celsius_to_fahrenheit(), f"8°C is 46.4°F")
